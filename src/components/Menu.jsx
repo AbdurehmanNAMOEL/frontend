d71a496816c1d12564d6b74fc9371db7304d1370
Abdurehman Saeed <asaeed526@gmail.com>
2022-11-09 09:48:26 +0300

    login edited

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,7 +14,7 @@ const Menu = ({isMenu,displayMenuBar,profileImage,toggleClicked}) => {
   <div className="w-40">
        <img src={Logo} alt="" className='h-16' />
    </div>
-    <div className='cursor-pointer -ml-[150px] h-[70px] flex justify-center items-center hover:text-[#4cebe3]'>
+    <div className='user-image cursor-pointer -ml-[150px] h-[70px] flex justify-center items-center hover:text-[#4cebe3]'>
           <img src={profileImage ? profileImage:User} alt="" className='w-[40px] h-[40px] rounded-full border-2 border-[#1212129f]' />
      </div>
 
@@ -28,12 +28,14 @@ const Menu = ({isMenu,displayMenuBar,profileImage,toggleClicked}) => {
     </div>
 
     { isMenu && <motion.div 
-               className=" rounded-sm fixed  gap-4 top-[75px] right-0 flex justify-center items-center flex-col w-[70%] h-[300px] bg-white shadow-md"
-               initial={{x:-200}}
-               animate={{x:-105}}
+               className=" mini-container rounded-sm fixed gap-4 top-[75px] right-[90px] flex justify-center items-center flex-col w-[70%] h-[300px] bg-white shadow-md"
+               initial={{y:-400}}
+               animate={{y:0}}
                transition={{type:'spring'}}
     >
-     <span  className='cursor-pointer flex justify-start items-center w-full h-12 hover:bg-green-300'><Link to={'/'} className='ml-[30px]'>Home</Link></span>
+     <span  className='cursor-pointer flex justify-start items-center w-full h-12 hover:bg-green-300'>
+       <Link className='ml-[30px]' to={`${profileImage?'/Home': '/'}`}>{`${profileImage?'DashBoard': 'Home'}`}</Link>
+    </span>
      <span   className='bg-white cursor-pointer justify-start flex  items-center w-full h-8 hover:bg-green-300 '><Link to={'/upload'} className='ml-[30px]'>Upload</Link></span>
      <span  className='cursor-pointer flex justify-start items-center w-full hover:bg-green-300 h-8'><Link  to={'/contactUs'} className='ml-[30px]'>Contact Us</Link></span>
      <span   className='cursor-pointer flex justify-start items-center w-full hover:bg-green-300 h-8'><Link to={'/aboutUs'} className='ml-[30px]'>About Us</Link></span>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,7 @@ import {toast} from 'react-toastify'
 import { logIn } from '../redux/features/authSlice'
 import Navbar2 from '../components/NavBar'
 import { useEffect } from 'react'
+import '../style/login.css'
 const Login = () => {
 
    const {user} = useSelector(state=>state.auth)
@@ -30,19 +31,19 @@ const Login = () => {
    },[])
  
   return (
-    <div className='w-[100%] flex flex-col h-[100vh] md:h-[100vh]  shadow-md rounded-md border-2 bg-[#292828d8] border-[#12121244]'>
-      <div className='w-full '>
+    <div className='w-[100%] overflow-hidden h- flex flex-col justify-center items-center   shadow-md rounded-md bg-[#292828d8] '>
+      <div className='w-full h-[70px] mb-10'>
       <Navbar2 path={'/home'}/>
       </div>
       
-      <div className='w-full  md:h-full flex flex-col md:flex-row '>
-          <img src={Stone} alt ='' className='w-full h-[250px] md:w-[50%] md:h-full bg-[#15f1a8] flex flex-col'/>
-      <div className='md:w-[50%] -mt-10 md:mt-0 h-full flex justify-center '>
+     <div className='login w-[100%] h-[100vh] gap-[20px] md:h-[80vh] flex flex-col md:flex-row'>
+          <img src={Stone} alt ='' className='w-full h-[300px] md:w-[50%] md:h-full'/>
+      <div className='h-[50vh] md:h-full md:w-[50%] -mt-10 md:mt-0  flex justify-center '>
        <div className='w-[400px] mt-[100px] h-[60%] items-center flex flex-col'>
-       <div className='h-[60px] w-full mt-10  flex justify-center items-center'>
-       <span className='text-xl text-white font-bold -mt-14'>LogIn</span>
+       <div className='h-[60px] w-full -mt-16 mb-10   flex justify-center items-center'>
+       <span className='text-xl text-white font-bold '>LogIn</span>
        </div>
-       <div className=' w-[50%] md:w-[80%] -mt-8 mb-8 md:mb-12 h-[1px] bg-[#4b4949]'/>
+       <div className='-mt-10 mb-5 w-[50%] md:w-[80%]  md:mb-12 h-[1px] bg-[#4b4949]'/>
       
        <InputField 
        type='email' 
@@ -64,12 +65,12 @@ const Login = () => {
        error={'valid password is required'}
        />
 
-      <span className='w-full flex ml-[290px]'>
+      <span className='sign-up w-full flex ml-[290px]'>
         <Link to={'/signup'} className='text-sm text-blue-500 -ml-16 md:ml-0'>
           you don't have an account? SignUp
         </Link>
       </span>
-      <button onClick={handleSubmit} id='btn-login' className='w-[80%] h-[40px] mt-4 flex justify-center items-center bg-blue-400 text-white rounded-md hover:bg-blue-500'>Login</button>
+      <button onClick={handleSubmit} id='btn-login' className='w-[80%] h-[40px]  mt-4 flex justify-center items-center bg-blue-400 text-white rounded-md hover:bg-blue-500'>Login</button>
     </div>
         </div>
       
