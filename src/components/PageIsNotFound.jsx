028c123ea9dd384b9e40af132cf94e05902a2a06
Abdurehman Saeed <asaeed526@gmail.com>
2022-11-11 13:17:57 +0300

    edited

--- src/components/PageIsNotFound.jsx
+++ src/components/PageIsNotFound.jsx
@@ -3,8 +3,8 @@ import Image from '../assets/Images/pagenotfound.gif';
 const PageIsNotFound = () => {
   return (
    <div className='w-full h-[100vh]'>
-    <img src={Image} alt="not-found" className='w-full h-[80%] md:h-full z-10' />
-    <div className='w-full absolute z-50 text-3xl font-bold flex items-center justify-center -mt-[5%] text-[rgba(55,57,57,0.7)]'>Page is not Found</div>
+    <img src={Image} alt="not-found" className='w-full h-full z-10' />
+    <div className='w-full absolute z-50 text-3xl font-bold flex items-center justify-center -mt-[20%] md:-mt-10% text-[rgba(55,57,57,0.7)]'>Page is not Found</div>
     </div>
   )
 }
