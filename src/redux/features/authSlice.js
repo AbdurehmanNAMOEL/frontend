9ca58bf66ca877bd1e3bf8cdb4ce8ab4c0528139
Abdurehman Saeed <asaeed526@gmail.com>
2022-12-03 12:25:42 +0300

    edited

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -9,7 +9,7 @@ axios.interceptors.request.use((req)=>{
   return req;
 })
 
-const basicUrl= 'https://web-production-e9b8.up.railway.app/'
+const basicUrl= 'https://seller-site.herokuapp.com/'
 
 export const createAccount =createAsyncThunk("user/createAccount",async({userData,toast,navigate})=>{
    
